75d0615110810a99ef1fef7cb473c8ddef80230c
Toemen <clems.steph@hotmail.fr>
2021-02-06 20:49:04 +0100

    commit

--- myPWA/views.py
+++ myPWA/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import Task
 
 def index(request):
+    tasks = Task.objects.all()
+    print(tasks[0].title)
+    data = {}
+    for t in tasks:
+        if t.beginning.month in data.keys():
+            data[t.beginning.month][t.beginning.day] = t.title
+        else:
+            data[t.beginning.month] = {t.beginning.day: t.title}
     return render(request, 'myPWA/index.html', locals())
